1f022f1c9fbbb1faeeff58a65f15a491a646b2b4
LostElk <lostelk.email@gmail.com>
2024-09-20 20:37:47 +0800

    chore(custom): 配置 FontAwesome

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <div>
     <ButtonDemo></ButtonDemo>
     <CollapseDemo></CollapseDemo>
+    <fontAwesomeIcon :icon="['fas', 'download']" />
   </div>
 </template>
 
 <script setup lang="ts">
 import ButtonDemo from '@/demo/Button.vue'
 import CollapseDemo from '@/demo/Collapse.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 </script>
 
 <style scoped></style>
