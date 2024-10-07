55845ecd9536c0bffb13c0af44fe699ccf6b98dc
LostElk <lostelk.email@gmail.com>
2024-10-08 01:40:35 +0800

    chore(custom): 测试配置 增加别名@

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom'
