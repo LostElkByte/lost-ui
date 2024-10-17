760fc0b60c4cc9015d6b00070a4d7ae7a544229f
LostElk <lostelk.email@gmail.com>
2024-10-17 23:54:12 +0800

    fix(custom): 修复组件按需引入名称

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,16 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-import Button from '@/components/Button'
-import Alert from '@/components/Alert'
-import Collapse, { CollapseItem } from '@/components/Collapse'
-import Dropdown from '@/components/Dropdown'
-import Icon from '@/components/Icon'
-import Message, { createMessage, closeAll as closeMessageAll } from '@/components/Message'
-import Tooltip from '@/components/Tooltip'
+import LButton from '@/components/Button'
+import LAlert from '@/components/Alert'
+import LCollapse, { CollapseItem as LCollapseItem } from '@/components/Collapse'
+import LDropdown from '@/components/Dropdown'
+import LIcon from '@/components/Icon'
+import LMessage, { createMessage, closeAll as closeMessageAll } from '@/components/Message'
+import LTooltip from '@/components/Tooltip'
 import './styles/index.css'
 
 library.add(fas)
-const components = [
-  Button,
-  Alert,
-  Collapse,
-  CollapseItem,
-  Dropdown,
-  Icon,
-  Message,
-  Notification,
-  Tooltip
-]
+const components = [LButton, LAlert, LCollapse, LCollapseItem, LDropdown, LIcon, LMessage, LTooltip]
 const install = (app: any) => {
   components.map((component) => {
     app.component(component.name, component)
@@ -28,14 +18,14 @@ const install = (app: any) => {
 }
 export {
   install,
-  Button,
-  Alert,
-  Collapse,
-  CollapseItem,
-  Dropdown,
-  Icon,
-  Message,
-  Tooltip,
+  LButton,
+  LAlert,
+  LCollapse,
+  LCollapseItem,
+  LDropdown,
+  LIcon,
+  LMessage,
+  LTooltip,
   createMessage,
   closeMessageAll
 }
